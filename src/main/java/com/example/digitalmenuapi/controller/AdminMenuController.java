54fa91700c5647287524b01d7bd8fb572dff5baa
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-05-05 12:29:33 +0100

    created controller layer of p2.

---
+++ src/main/java/com/example/digitalmenuapi/controller/AdminMenuController.java
@@ -0,0 +1,71 @@
+package com.example.digitalmenuapi.controller;
+
+import com.example.digitalmenuapi.model.AdminMenuItems;
+import com.example.digitalmenuapi.service.AdminMenuItemsService;
+import java.net.URI;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * admin controller class.
+ */
+@RestController
+@RequestMapping("/admin")
+public class AdminMenuController {
+
+  @Autowired
+  AdminMenuItemsService adminMenuItemsService;
+
+  public AdminMenuController(AdminMenuItemsService adminMenuItemsService)  {
+    this.adminMenuItemsService = adminMenuItemsService;
+  }
+
+  Logger logger = LoggerFactory.getLogger(MenuItemsController.class);
+
+  /**
+   * Post request method to add new sandwiches to the menu.
+   */
+  @PostMapping("/menu")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<AdminMenuItems> addMenu(@RequestBody AdminMenuItems adminMenuItems) {
+    AdminMenuItems addedMenu = adminMenuItemsService.createNewSandwiches(adminMenuItems);
+    logger.info("Sandwich created successfully");
+    return ResponseEntity.created(URI.create("admin/menu/" + addedMenu.getId())).body(addedMenu);
+  }
+
+  /**
+   * Delete request method to delete sandwiches from the menu
+   * providing the correct id.
+   */
+  @DeleteMapping()
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<String> deleteMenu(@RequestParam String id) {
+    adminMenuItemsService.deleteMenuItem(id);
+    logger.info("Sandwich deleted successfully");
+    return ResponseEntity.ok().body("Deleted successfully");
+  }
+
+  /**
+   * Get request method to get all temporary unavailable sandwiches from the menu.
+   */
+  @GetMapping("/unavailable")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<List<AdminMenuItems>> getAllTemporaryUnavailableSandwiches() {
+    logger.info("All temporary unavailable sandwiches retrieved successfully");
+    List<AdminMenuItems> menuList = adminMenuItemsService.getAllTemporaryUnavailableSandwiches();
+    return new ResponseEntity<>(menuList, HttpStatus.OK);
+  }
+}
+
