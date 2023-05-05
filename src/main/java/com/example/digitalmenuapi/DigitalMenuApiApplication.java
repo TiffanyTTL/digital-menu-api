ab3cbd503b6dcefad89e03a9f66627320eec949f
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-05-05 12:30:10 +0100

    added java doc comments

---
+++ src/main/java/com/example/digitalmenuapi/DigitalMenuApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.digitalmenuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Digital Menu Api SpringBoot Application class.
+ */
+@SpringBootApplication
+public class DigitalMenuApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DigitalMenuApiApplication.class, args);
+  }
+
+}
