a7df174c3a86430f514db58502828afb07ad3e42
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-05-05 11:32:01 +0100

    TDD - for task 3 for p2 completed

--- src/test/java/com/example/digitalmenuapi/controllerTest/adminControllerTest.java
+++ src/test/java/com/example/digitalmenuapi/controllerTest/adminControllerTest.java
@@ -2,14 +2,13 @@ package com.example.digitalmenuapi.controllerTest;
 
 import com.example.digitalmenuapi.controller.AdminMenuController;
 import com.example.digitalmenuapi.model.AdminMenuItems;
+import com.example.digitalmenuapi.model.MenuItems;
 import com.example.digitalmenuapi.service.AdminMenuItemsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-//import org.springframework.security.test.context.support.WithMockUser;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,8 +16,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -105,9 +110,43 @@ public class adminControllerTest {
                             .contentType(MediaType.APPLICATION_JSON))
                     .andDo(print())
                     .andExpect(status().isOk());
-
-
         }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void getAllTemporaryUnavailableSandwichesInAListTest() throws Exception {
+        List<AdminMenuItems> adminMenuItems = Arrays.asList(
+                new AdminMenuItems("Bacon Egg And Cheese Sandwich", 970,
+                        "wheat, egg, cheese, mustard", false, false, 12.50, false),
+                new AdminMenuItems("Tofu crab Sandwich", 500, "soy beans, wheat", true,
+                        true, 18.50, false));
+        given(adminMenuItemsService.getAllTemporaryUnavailableSandwiches()).willReturn(adminMenuItems);
+        String json = objectMapper.writeValueAsString(adminMenuItems);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("admin123", "staff");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/unavailable")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Bacon Egg And Cheese Sandwich"))
+                .andExpect(jsonPath("$[0].calories").value(970))
+                .andExpect(jsonPath("$[0].allergies").value("wheat, egg, cheese, mustard"))
+                .andExpect(jsonPath("$[0].vegan").value(false))
+                .andExpect(jsonPath("$[0].vegetarian").value(false))
+                .andExpect(jsonPath("$[0].price").value(12.50))
+                .andExpect(jsonPath("$[0].available").value(false))
+                .andExpect(jsonPath("$[1].name").value("Tofu crab Sandwich"))
+                .andExpect(jsonPath("$[1].calories").value(500))
+                .andExpect(jsonPath("$[1].allergies").value("soy beans, wheat"))
+                .andExpect(jsonPath("$[1].vegan").value(true))
+                .andExpect(jsonPath("$[1].vegetarian").value(true))
+                .andExpect(jsonPath("$[1].price").value(18.50))
+                .andExpect(jsonPath("$[1].available").value(false));
+
+    }
     }
 
 
