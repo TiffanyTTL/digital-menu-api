1c8fffbb938ce50d52b57257adfb4d7b10db2cb3
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-05-05 11:03:44 +0100

    TDD - for task 2 for p2 completed

--- src/test/java/com/example/digitalmenuapi/controllerTest/adminControllerTest.java
+++ src/test/java/com/example/digitalmenuapi/controllerTest/adminControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 //import org.springframework.security.test.context.support.WithMockUser;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -77,5 +79,35 @@ public class adminControllerTest {
                 .andExpect(jsonPath("$.vegan").value(false))
                 .andExpect(jsonPath("$.vegetarian").value(false));
     }
-}
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    public void deleteMenuItemFromDatabase() throws Exception {
+            AdminMenuItems adminMenuItems2 = new AdminMenuItems();
+            adminMenuItems2.setId("6454d1411fc1e574b1439688");
+            adminMenuItems2.setName("Tofu Steak Sandwich");
+            adminMenuItems2.setCalories(1300);
+            adminMenuItems2.setAllergies("wheat, poultry, chicken");
+            adminMenuItems2.setPrice(9.50);
+            adminMenuItems2.setAvailable(true);
+            adminMenuItems2.setVegan(false);
+            adminMenuItems2.setVegetarian(false);
+            when(adminMenuItemsService.deleteMenuItem("6454d1411fc1e574b1439688")).thenReturn("Deleted successfully");
+            String json = objectMapper.writeValueAsString(adminMenuItems2);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBasicAuth("admin123", "staff");
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            mockMvc.perform(delete("/admin")
+                            .param("id", "6454d1411fc1e574b1439688")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .headers(headers)
+                            .content(json)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+
+
+        }
+    }
+
 
