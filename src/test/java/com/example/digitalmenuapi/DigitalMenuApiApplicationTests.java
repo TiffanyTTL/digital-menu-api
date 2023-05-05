fcdb875e3f08696398d1d72679f54ba35e60c13f
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-05-05 12:30:31 +0100

    commits

---
+++ src/test/java/com/example/digitalmenuapi/DigitalMenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitalmenuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
