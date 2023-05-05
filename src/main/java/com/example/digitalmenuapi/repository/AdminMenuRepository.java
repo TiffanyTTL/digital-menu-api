78aea351a514c99e9510bf89ff490381234b44a7
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-05-05 12:30:41 +0100

    created repo

---
+++ src/main/java/com/example/digitalmenuapi/repository/AdminMenuRepository.java
@@ -0,0 +1,14 @@
+package com.example.digitalmenuapi.repository;
+
+import com.example.digitalmenuapi.model.AdminMenuItems;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+/**
+ * Admin menu repository class.
+ */
+public interface AdminMenuRepository extends MongoRepository<AdminMenuItems, String> {
+
+  List<AdminMenuItems> findAdminMenuItemsByAvailable(boolean available);
+}
